90311a0e48b2250119a763bd1f034b52e216cd2a
David Currie <davidalexandercurrie@gmail.com>
2021-02-22 00:10:01 -0500

    noun detection

--- server.js
+++ server.js
@@ -7,6 +7,12 @@ const options = {
 };
 const io = require('socket.io')(http, options);
 
+const WordPOS = require('wordpos'),
+  wordpos = new WordPOS();
+
+const natural = require('natural');
+const tokenizer = new natural.WordTokenizer();
+
 app.use('/', express.static('public'));
 
 const listener = http.listen(process.env.PORT || 3000, process.env.IP, () => {
@@ -17,8 +23,21 @@ io.on('connection', socket => {
   console.log('User connected! Their ID is ' + socket.id);
 
   socket.on('messageFromUser', msg => {
-    console.log('The message from User is ' + msg);
-
-    socket.broadcast.emit('messageFromServer', msg);
+    // console.log('The message from User is ' + msg);
+    let arr = tokenizer.tokenize(msg);
+    let counter = 0;
+    for (let i = 0; i < arr.length; i++) {
+      wordpos.isNoun(arr[i], function (result) {
+        if (result) {
+          arr[i] = 'pizza';
+        }
+        counter++;
+        if (counter === arr.length) {
+          let words = arr.join(' ');
+          console.log(words);
+          socket.broadcast.emit('messageFromServer', words);
+        }
+      });
+    }
   });
 });
